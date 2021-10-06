6b93ad741c325e5026917ed2ab0afd1a67498f6a
Caitlin Robertson <robertson0630@gmail.com>
2021-10-06 11:18:53 -0500

    Created script.js

---
+++ script.js
@@ -0,0 +1,84 @@
+// 1a
+const mcuShows = ["Loki", "What If...?"]
+
+// 1b
+const starWarsShows = ["The Mandalorian", "The Bad Batch"]
+
+// 1c
+const disneyPlusShows = [...mcuShows, ...starWarsShows, "Monsters At Work"]
+
+// 1d
+console.log(disneyPlusShows);
+
+// 2a
+const netflixMovies = {
+    action: "Extraction",
+    crime: "The Irishman"
+};
+
+// 2b
+const amazonPrimeMovies = {
+    action: "The Tomorrow War",
+    drama: "One Night In Miami"
+}
+
+// 2c
+const streamingMovies = {
+    ...amazonPrimeMovies,
+    ...netflixMovies,
+    musical: "Hamilton"
+}
+
+// 2d
+console.log(streamingMovies);
+
+// 3a
+const disneyJunior = ["Mickey Mouse Clubhouse", "Spidey and His Amazing Friends"];
+
+// 3b
+const [mickey, spidey] = disneyJunior;
+
+// 3c
+console.log(mickey, spidey);
+
+// 4a
+const avengers = {
+    warMachine: "James Rhodes",
+    theHulk: "Bruce Banner"
+}
+
+// 4b
+const {warMachine, theHulk} = avengers;
+
+// 4c
+console.log(avengers);
+
+// 4d
+const moreAvengers = {
+    blackWidow: "Natasha Romanoff",
+    hawkeye: "Clint Barton",
+    ironMan: "Tony Stark"
+};
+
+// 4e
+const {blackWidow: nat, ...other} = moreAvengers;
+
+// 4f
+console.log(moreAvengers);
+
+// BONUS
+const bonus = {
+    first: [1, 2, 3],
+    second: [4, 5, 6],
+    third: [7, 8, 9]
+  };
+
+//   5a
+const all = {
+    ...bonus["first"],
+    ...bonus["second"],
+    ...bonus["third"]
+};
+
+// 5b
+const [one, two, three, four, five, six, seven, eight, nine] = all; 
